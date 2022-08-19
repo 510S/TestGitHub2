844da621ffdda4e8e8378de77856505aaf854509
atsushi510 <g7552346s@gmail.com>
2022-08-19 18:19:45 +0900

    15

--- src/main/java/com/gocchan/testgithub2/TestGitHub2.java
+++ src/main/java/com/gocchan/testgithub2/TestGitHub2.java
@@ -4,10 +4,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TestGitHub2 extends JavaPlugin {
 
-<<<<<<< HEAD
-}
-=======
 }
 
 // THIS555
->>>>>>> branch 'master' of git@github.com.2:atsushi510/TestGitHub2.git
